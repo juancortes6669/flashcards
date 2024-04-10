a7fb68d4dcb1b4845fb1f1854a9e2e03f117740d
juancortes6669 <juancortes321@gmail.com>
2024-04-09 20:12:58 -0500

    nodeimffff

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const fetch = require('node-fetch');
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { join } from 'path';
+import path from 'path';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
